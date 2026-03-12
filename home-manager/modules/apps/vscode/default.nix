1edc66b0aa05a25705da4304fa261bc727178ced
sulaimanmugahed <hg123om5@gmail.com>
2026-03-13 02:07:53 +0300

    jffh

--- home-manager/modules/apps/vscode/default.nix
+++ home-manager/modules/apps/vscode/default.nix
@@ -8,9 +8,30 @@ let
   marketplace = pkgs.vscode-utils.buildVscodeMarketplaceExtension;
   defaultExtensions = with pkgs.vscode-extensions; [
     jnoortheen.nix-ide
-    ms-dotnettools.csharp
-    ms-dotnettools.csdevkit
-    ms-dotnettools.vscode-dotnet-runtime
+    (marketplace {
+      mktplcRef = {
+        publisher = "ms-dotnettools";
+        name = "csharp";
+        version = "2.120.3";
+        sha256 = "sha256-dRNmjuV2lwa9Xe0M4LDOvCW80gVA1NcZHNeo7E4rqMo=";
+      };
+    })
+    (marketplace {
+      mktplcRef = {
+        publisher = "ms-dotnettools";
+        name = "csdevkit";
+        version = "2.10.3";
+        sha256 = "sha256-8oxIWjOCee5nxlJZZ58TZqNxZUrGltQnqr0L4u7LMNY=";
+      };
+    })
+    (marketplace {
+      mktplcRef = {
+        publisher = "ms-dotnettools";
+        name = "vscode-dotnet-runtime";
+        version = "3.0.0";
+        sha256 = "sha256-RA7skgj6yFZxk2XuJZtcDrI4dFrAbwODmwqSx4xWFUY=";
+      };
+    })
     streetsidesoftware.code-spell-checker
   ];
 
